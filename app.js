34444dac28308bd8eb6858fa8e7c99ea136f1bc2
Michael Mallari <hello@michaelmallari.com>
2018-09-14 12:24:08 -0400

    Added Heroku dependencies

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -20,7 +20,10 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use('/users', usersRouter);
+
+// React app build
+app.use('/app', express.static(__dirname + '/react_client/build'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
